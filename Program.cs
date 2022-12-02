8164ffb405905388ea8d68d73dc9378def407869
bigpapiem <119636238+bigpapiem@users.noreply.github.com>
2022-12-02 09:46:20 -0500

    Update Program.cs

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using System; /* Grading ID:S0287, Lab 1, 1/24/2020, CIS 199-02, Program code outputs typed information of hobbies, fav book and fav movie*/
+
+namespace Lab1__1_
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Grading ID: S0287");
+			Console.WriteLine("Hobbies: Video games, music listening and writing poetry");
+			Console.WriteLine("Fvaorite Book: The Sun Also Rises");
+			Console.WriteLine("Favorite Movie: Donnie Darko");
+		}
+	}
+}
